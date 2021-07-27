c6b58ab5cedd3006e65237ec87c9cbcd6fa2fa31
Guillaume Clement <guillaumecle.me@gmail.com>
2021-07-26 21:27:21 -0600

    Fixing og image and hero

--- src/components/Sections/MainHero.vue
+++ src/components/Sections/MainHero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative pt-16 pb-32 flex content-center items-center justify-center" style="min-height: 75vh;">
     <CoverImage
-      cover="https://images.unsplash.com/photo-1483422166412-1d8a821cd97c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+      cover="https://images.unsplash.com/photo-1627350898251-61e79df3fbbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80"
     />
     <div class="container relative mx-auto">
       <div class="items-center flex flex-wrap">
