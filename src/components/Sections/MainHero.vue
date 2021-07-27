06ec83fc6070b4773d80dc2e787029737182a134
Guillaume Clement <guillaumecle.me@gmail.com>
2021-07-27 17:58:50 -0600

    Hero quick fix

--- src/components/Sections/MainHero.vue
+++ src/components/Sections/MainHero.vue
@@ -6,14 +6,12 @@
     <div class="container relative mx-auto">
       <div class="items-center flex flex-wrap">
         <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-          <div class="pr-12">
-            <h1 class="text-white text-5xl font-serif uppercase">
-              NOMAD•IFY
-            </h1>
-            <p class="mt-4 text-2xl text-gray-300 font-serif">
-              A blog about digital nomadism and minimalism.
-            </p>
-          </div>
+          <h1 class="text-white text-5xl font-serif uppercase">
+            NOMAD•IFY
+          </h1>
+          <p class="mt-4 text-2xl text-gray-300 font-serif">
+            A blog about digital nomadism and minimalism.
+          </p>
         </div>
       </div>
     </div>
